1d6e94ab48dd2e6e56aefcb62aea99a10db731ba
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-10-17 13:35:11 -0700

    questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,40 +1,53 @@
-const questions = [
+module.exports = {
+    typeofEmployee: [
+        {
+            type: 'list',
+            message: 'What type of employee?',
+            name: 'employee',
+            choices: ["Manager", "Engineer", "Intern"],
+            validate: (value) => { if (value) { return true; } else { return "Select response to continue"; } },
 
-    {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'name',
-        validate: (value) => { if (value) { return true; } else { return "Enter response to continue"; } },
-    },
-    {
-        type: 'input',
-        message: 'What is your ID number?',
-        name: 'id',
-        validate: (value) => { if (value) { return true; } else { return "Enter response to continue"; } },
-    },
-    {
-        type: 'input',
-        message: 'Enter your email:',
-        name: 'email',
-        validate: (value) => { if (value) { return true; } else { return "Enter email to continue"; } },
-    },        
-    {
-        type: 'input',
-        message: 'Enter you GitHub username:',
-        name: 'github',
-        validate: (value) => { if (value) { return true; } else { return "Enter your github username to continue"; } },
-    },
-    {
-        type: 'input',
-        message: 'Enter your office number:',
-        name: 'office',
-        validate: (value) => { if (value) { return true; } else { return "Enter your office number to continue"; } },
-    },
-    {
-        type: 'input',
-        message: 'Enter your school name:',
-        name: 'school',
-        validate: (value) => { if (value) { return true; } else { return "Enter your school name to continue"; } },
-    },
+        }
+    ],
+    generalQuestions : [
+            {
 
-]
+            },
+            {
+                type: 'input',
+                message: 'What is your ID number?',
+                name: 'id',
+                validate: (value) => { if (value) { return true; } else { return "Enter response to continue"; } },
+            },
+            {
+                type: 'input',
+                message: 'Enter your email:',
+                name: 'email',
+                validate: (value) => { if (value) { return true; } else { return "Enter email to continue"; } },
+            },  
+    ],     
+    gitHubUserName : [ 
+        {
+            type: 'input',
+            message: 'Enter you GitHub username:',
+            name: 'github',
+            validate: (value) => { if (value) { return true; } else { return "Enter your github username to continue"; } },
+        },
+    ],
+    officeNumber: [
+        {
+            type: 'input',
+            message: 'Enter your office number:',
+            name: 'office',
+            validate: (value) => { if (value) { return true; } else { return "Enter your office number to continue"; } },
+        },
+    ],
+    schoolName: [ 
+        {
+            type: 'input',
+            message: 'Enter your school name:',
+            name: 'school',
+            validate: (value) => { if (value) { return true; } else { return "Enter your school name to continue"; } },
+        },
+    ],
+}
