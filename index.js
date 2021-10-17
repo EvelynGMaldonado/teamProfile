269f77424fa8cb1c682dac905baa7b435a106a8b
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-10-17 14:24:13 -0700

    employee and manager are working now

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+
 // const Employee = require("./lib/Employee");
 // const Engineer = require("./lib/Engineer");
 // const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager");
+const Manager = require("./lib/Manager");
 const questions = require("./utils/questions");
 const { generalQuestions } = require('./utils/questions');
 
@@ -16,7 +17,10 @@ const init = async () => {
 const collectEmplData = async (typeEmp) => {
     console.log(typeEmp)
     const genEmpInfo = await inquirer.prompt(questions.generalQuestions);
-    console.log("general info");
+    if (typeEmp.employee === "Manager") {
+        const officeNumb = await inquirer.prompt(questions.officeNumber);
+        console.log("the office number is ", officeNumb);
+    } 
 }
 
 init ();   
